35af78f9e2748b54545fd3531761cfc9f30a99a5
Rainbrand <26818172+Rainbrand@users.noreply.github.com>
2021-05-18 02:03:37 +0500

    Added correct user grouping and group hiding on click

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,23 +1,82 @@
-import React, {FC, useContext} from 'react';
+import React, {FC, useContext, useEffect, useState} from 'react';
 import {UsersContext} from "./UserDataProvider"
 import {IUser} from "../types/types";
 import "./Search.scss"
 
 const Search: FC = () => {
-    const context = useContext<IUser[]>(UsersContext)
+    const groupedUsersList: Map<number, IUser[]> = useContext(UsersContext)
+    const [isHiddenState, setHiddenState] = useState<Array<boolean>>([] as Array<boolean>);
+
+    useEffect(() => {
+        setHiddenState(initIsHiddenState)
+        console.log(isHiddenState)
+    }, [groupedUsersList]);
+
+
+    const temp = Array.from(groupedUsersList)
+
+    const initIsHiddenState = () => {
+        const state = new Array<boolean>()
+        for (let i = 1; i <=10; i++){
+            state[i] = false
+        }
+        return state
+    }
+
+    const updateHiddenState = (index: number) => {
+        setHiddenState(prev => {
+            const newState = [...prev]
+            newState[index] = !newState[index]
+            return newState
+        })
+    }
+
+
+    // const updateHiddenState = (index: number) => {
+    //     setHiddenState(prev => {
+    //         prev[index] = !prev[index]
+    //         return prev;
+    //         }
+    //     )
+    // }
+    //
+    // const renderUsers = (value) => {
+    //     return (value[1].map(user => (
+    //         <li key={user.id.value} className='search__user'>
+    //             {`${user.name.first} ${user.name.last}`}
+    //         </li>
+    //     )))
+    // }
 
     return (
         <div className="search" >
             <span className="search__input" contentEditable="true"/>
-            <ul className="search__list">
-                {context.length === 0 ? "Loading..." :
-                      context.map(user => (
-                      <li key={user.id.value} className="search__item">
-                          {`${user.name.first} ${user.name.last}`}
-                      </li>
-                 ))}
-
-            </ul>
+            <ol className="search__groups">
+                {temp.map(value => (
+                    <li key={value[0]} className={`search__group ${value[1].length === 0 ? 'search__group--disabled' : 'search__group--active'}`}>
+                        <ul>
+                            <div className="search__group__name" onClick={() => updateHiddenState(value[0])}>
+                                {`${value[0] + 9 * (value[0] - 1)}-${value[0] * 10}`}
+                            </div>
+                            {isHiddenState[value[0]] ? value[1].map(user => (
+                                <li key={user.id.value} className='search__user'>
+                                    {`${user.name.first} ${user.name.last}`}
+                                </li>)) : null}
+                            {/*{value[1].map(user => (*/}
+                            {/*    <li key={user.id.value} className='search__user'>*/}
+                            {/*        {`${user.name.first} ${user.name.last}`}*/}
+                            {/*    </li>*/}
+                            {/*))}*/}
+                        </ul>
+                    </li>
+                ))}
+                {/*{groupedUsersList.length === 0 ? "Loading..." :*/}
+                {/*    groupedUsersList.map(group => (*/}
+                {/*      <ul key={group.key()} className="search__group">*/}
+                {/*          {`${user.name.first} ${user.name.last}`}*/}
+                {/*      </ul>*/}
+                {/* ))}*/}
+            </ol>
         </div>
     )
 }
--- src/components/UserDataProvider.tsx
+++ src/components/UserDataProvider.tsx
@@ -2,20 +2,39 @@ import React, {FC, useEffect, useState} from 'react';
 import axios from "axios";
 import {IChildren, IFetch, IUser} from "../types/types";
 
-const UsersContext = React.createContext<IUser[]>({} as IUser[])
+const UsersContext = React.createContext<Map<number, IUser[]>>({} as Map<number, IUser[]>)
 
 const UserDataProvider: FC<IChildren> = (props) => {
-    const [userData, setUserData] = useState<IUser[]>([]);
+    const [markedUsers, setMarkedUsers] = useState<Map<number, IUser[]>>({} as Map<number, IUser[]>);
+    //const [markedUsers, setMarkedUsers] = useState([]);
 
     async function getUsers() {
         try {
-            const users = await axios.get<IFetch>('https://randomuser.me/api/?results=2' +
+            const users = await axios.get<IFetch>('https://randomuser.me/api/?results=10' +
                 '&inc=name,email,registered,picture,id' +
                 '&nat=us,gb')
-            await setUserData(users.data.results)
+            await tagUsers(users.data.results)
         } catch (e){
             console.error(e)
         }
+    }
+
+    async function tagUsers(users: IUser[]){
+        const groupedUsers = new Map<number, IUser[]>()
+        for (let i = 1; i <= 10; i++){
+            groupedUsers.set(i, [])
+        }
+        for (const user of users) {
+            const age = Math.floor((user.registered.age - 1) / 10) + 1
+            const alreadyGrouped = groupedUsers.get(age)
+            if (alreadyGrouped === undefined) {
+                groupedUsers.set(age, [user]);
+            } else {
+                groupedUsers.set(age, [...alreadyGrouped, user])
+            }
+        }
+        await setMarkedUsers(groupedUsers)
+        console.log(groupedUsers)
     }
 
     useEffect(() => {
@@ -24,7 +43,7 @@ const UserDataProvider: FC<IChildren> = (props) => {
 
     
     return (
-        <UsersContext.Provider value={userData}>
+        <UsersContext.Provider value={markedUsers}>
             {props.children}
         </UsersContext.Provider>
     )
